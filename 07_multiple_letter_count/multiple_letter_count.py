5eb51a9710d16901ac20b64d497bf37e387cf8f7
david <davidmcconkey@outlook.com>
2023-01-19 15:52:13 -0500

    Uplading exercises

---
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,14 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    newDict = {}
+    for ltr in phrase:
+        newDict[ltr] = newDict.get(ltr, 0) + 1
+
+    return newDict
